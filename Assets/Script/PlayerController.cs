608c09f9beef46d289df4971caa84c2a9a29c015
Like-Hero <1512924018@qq.com>
2021-06-04 21:28:39 +0800

    新增趴下Crouch动画,更新地图,解决平地跑动可能出现的卡顿问题

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,21 +6,27 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private Collider2D collider;
+    public Collider2D downCollider;
+    public Collider2D topCollider;
 
     private Rigidbody2D rb;
     private Animator anim;
     public Text cherryValue;
     public LayerMask ground;
+    public LayerMask obstruct;
+
 
     public AudioSource hurtAudio;
     public AudioSource jumpAudio;
     public AudioSource cherryAudio;
 
+    public Transform cellingPoint;
+
     public float speed;
     public float jumpforce;
     public int cherryAmount;
+
+    public const float CROUCH_RATE = 0.3f;
 
     private void Start()
     {
@@ -29,7 +35,7 @@ public class PlayerController : MonoBehaviour
         cherryAmount = 0;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        collider = GetComponent<CircleCollider2D>();
+        downCollider = GetComponent<CircleCollider2D>();
     }
 
     private void Update()
@@ -61,17 +67,25 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(facedDir, 1, 1);
         }
-        //改变x速度
-        anim.SetFloat("running", Mathf.Abs(xVelocity));
-        if (xVelocity != 0)
+        //改变x速度,如果为真,则改变速度
+        if (Crouch())
         {
-            rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+            rb.velocity = new Vector2(xVelocity * speed * CROUCH_RATE, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(xVelocity * CROUCH_RATE));
+        }
+        else
+        {
+            anim.SetFloat("running", Mathf.Abs(xVelocity));
+            if (xVelocity != 0)
+            {
+                rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+            }
         }
     }
     private void YMove()
     {
         //改变y速度
-        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow)) && collider.IsTouchingLayers(ground))
+        if (Input.GetButtonDown("Jump") && (downCollider.IsTouchingLayers(ground) || downCollider.IsTouchingLayers(obstruct)))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
             anim.SetBool("falling", false);
@@ -85,7 +99,7 @@ public class PlayerController : MonoBehaviour
             anim.SetBool("jumping", false);
             //anim.SetBool("idle", false);
         }
-        if (collider.IsTouchingLayers(ground))
+        if (downCollider.IsTouchingLayers(ground) || downCollider.IsTouchingLayers(obstruct))
         {
             anim.SetBool("falling", false);
             anim.SetBool("hurt", false);//掉落在地上恢复玩家对主角的控制权
@@ -132,5 +146,49 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(enemy.transform.position.x < this.transform.position.x ? 10 : -10, jumpforce / 2);
         hurtAudio.Play();
+    }
+    private bool Crouch()
+    {
+        //如果是跳跃或者下落,则不可下蹲
+        if(anim.GetBool("jumping") || anim.GetBool("falling"))
+        {
+            return false;
+        }
+        else
+        {
+            //头上没东西
+            if (!Physics2D.OverlapCircle(cellingPoint.position, 0.2f, obstruct))
+            {
+                if (Input.GetButton("Crouch"))
+                {
+                    anim.SetBool("crouching", true);
+                    topCollider.enabled = false;
+                    return true;
+                }
+                else
+                {
+                    anim.SetBool("crouching", false);
+                    topCollider.enabled = true;
+                    return false;
+                }
+            }
+            else//头上有东西
+            {
+                print("OK");
+                //进一步判断是否是蹲下状态
+                if (anim.GetBool("crouching"))
+                {
+                    
+                    topCollider.enabled = false;
+                    return true;
+                }
+                else
+                {
+                    topCollider.enabled = true;
+                    return false;
+                }
+            }
+        }
+        
     }
 }
